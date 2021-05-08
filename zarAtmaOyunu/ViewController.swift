b70e5dc8d8018ff84395c9591fed65f76fadb101
SAIDE CEKİC <saidecekic@icloud.com>
2021-05-08 17:48:03 +0300

    Initial Commit

---
+++ zarAtmaOyunu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zarAtmaOyunu
+//
+//  Created by Saide Cekic on 8.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
